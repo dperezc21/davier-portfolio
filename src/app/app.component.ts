f03e2463c0de3db95c13c0f041dbe5140d0bf985
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-03-13 14:59:29 -0500

    feat: schema to title, profile and icons done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {ICONS_TECHNOLOGIES, IconsValue} from './constants/icons-path';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'davier-portfolio';
+  technologies: IconsValue[] = ICONS_TECHNOLOGIES;
 }
---
+++ src/app/constants/icons-path.ts
@@ -0,0 +1,15 @@
+export interface IconsValue {
+  path: string,
+  name: string
+}
+
+export const ICONS_TECHNOLOGIES: IconsValue[] = [
+  {
+    name: "java",
+    path: "/java.png"
+  },
+  {
+    name: "javascript",
+    path: "/js.png"
+  }
+]
