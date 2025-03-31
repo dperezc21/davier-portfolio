4bd450d45baa86ea72c704235d38442bc4b9c8a1
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-03-30 23:29:26 -0500

    feat: displaying contact information

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {ContactInformationComponent} from '../contact-information/contact-information.component';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [
+    ContactInformationComponent
+  ],
   templateUrl: './about.component.html',
   standalone: true,
   styleUrl: './about.component.css'
