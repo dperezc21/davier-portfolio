421305aa5f067bdf54937a3b5b1e657e4c522527
davier enrique perez cabrera <davierperez11.2@gmail.com>
2025-03-31 21:05:37 -0500

    feat: show the technologies that use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import {Component} from '@angular/core';
 import {HeaderComponent} from './components/header/header.component';
 import {RouterOutlet} from '@angular/router';
+import {MatTooltip} from '@angular/material/tooltip';
+import {ICONS_TECHNOLOGIES, IconsValue} from './constants/icons-path';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
-    RouterOutlet
+    RouterOutlet,
+    MatTooltip
   ],
   templateUrl: './app.component.html',
   standalone: true,
@@ -14,4 +17,5 @@ import {RouterOutlet} from '@angular/router';
 })
 export class AppComponent {
   title = 'davier-portfolio';
+  technologies: IconsValue[] = ICONS_TECHNOLOGIES;
 }
